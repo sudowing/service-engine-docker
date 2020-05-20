d4dbb918b56e3f2a3119ccb761823d055ab8d269
Joe Wingard <accounts@joewingard.com>
2020-05-19 20:57:42 -0400

    initial build (#1)
    we do it live :fire:

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import * as knex from "knex";
+import { ignite } from "service-engine";
+
+import * as knexConfig from "../knexfile";
+
+// const metadata = require("./metadata.json");
+
+const metadata = {
+  "appShortName": "some-app-service",
+  "title": "Some App Service",
+  "description": "Basic description of core resources.",
+  "termsOfService": "http://website.io/terms/",
+  "name": "Joe Wingard",
+  "email": "open-source@joewingard.com",
+  "url": "https://github.com/sudowing/service-engine",
+  "servers": [
+      "http://localhost:3001",
+      "https://alpha.com",
+      "https://bravo.com",
+      "https://charlie.com"
+  ]
+};
+
+const port = process.env.PORT || 3001;
+
+const db = knex(knexConfig);
+
+const main = async () => {
+  await db.migrate.latest();
+
+  const { App, logger } = await ignite({ db, metadata });
+
+  logger.info("DB Migrations Run 🔧");
+
+  App.listen({ port }, () => {
+    logger.info({ port }, "App Started 🔥");
+  });
+};
+
+main();
